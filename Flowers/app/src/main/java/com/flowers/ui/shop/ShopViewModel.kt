78243f37b1ba13c8bc80b2fe4e002385514a2bf2
Patryk <patryk.gielcz@gmail.com>
2022-04-23 12:51:01 +0200

    initial refactoring of shop activities

--- Flowers/app/src/main/java/com/flowers/ui/shop/ShopViewModel.kt
+++ Flowers/app/src/main/java/com/flowers/ui/shop/ShopViewModel.kt
@@ -1,10 +1,10 @@
-package com.flowers.ui.gallery
+package com.flowers.ui.shop
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class ShopViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is gallery Fragment"
