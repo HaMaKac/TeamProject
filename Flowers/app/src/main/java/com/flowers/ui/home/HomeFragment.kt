25b64b6a687fa3be3a246ffd0e8ab0eeee1c7c16
Patryk <patryk.gielcz@gmail.com>
2022-04-23 13:35:39 +0200

    names refactoring and changes in layout/textFields

--- Flowers/app/src/main/java/com/flowers/ui/home/HomeFragment.kt
+++ Flowers/app/src/main/java/com/flowers/ui/home/HomeFragment.kt
@@ -30,7 +30,7 @@ class HomeFragment : Fragment() {
 
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            textView.text = "Welcome to the main page"
         }
         return root
     }
--- Flowers/app/src/main/java/com/flowers/ui/plant/PlantFragment.kt
+++ Flowers/app/src/main/java/com/flowers/ui/plant/PlantFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.flowers.databinding.FragmentSlideshowBinding
+import com.flowers.databinding.FragmentPlantBinding
 
 class PlantFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
+    private var _binding: FragmentPlantBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class PlantFragment : Fragment() {
         val slideshowViewModel =
             ViewModelProvider(this).get(PlantViewModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentPlantBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textPlant
