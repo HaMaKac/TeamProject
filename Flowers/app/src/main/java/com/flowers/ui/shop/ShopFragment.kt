2ab16e9ad89d6cf88efbdff41c77608030af806a
Patryk <patryk.gielcz@gmail.com>
2022-05-11 18:25:36 +0200

    popup placeholder, works with first button

--- Flowers/app/src/main/java/com/flowers/ui/shop/ShopFragment.kt
+++ Flowers/app/src/main/java/com/flowers/ui/shop/ShopFragment.kt
@@ -1,12 +1,18 @@
 package com.flowers.ui.shop
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.flowers.R
 import com.flowers.databinding.FragmentShopBinding
 
 class ShopFragment : Fragment() {
@@ -32,11 +38,27 @@ class ShopFragment : Fragment() {
         shopViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+        initializeButtons()
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun initializeButtons() {
+        binding.button1.setOnClickListener {
+            showPopUp()
+        }
+    }
+
+    fun showPopUp() {
+        val dialogBuilder = AlertDialog.Builder(context)
+        dialogBuilder.setMessage("flower purchased")
+        dialogBuilder.setPositiveButton("Ok"
+        ) { dialog, whichButton -> }
+        val b = dialogBuilder.create()
+        b.show()
     }
 }
