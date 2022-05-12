dbffb3b3b70345b8972f4987032e398e61228f51
234758 Hanna Kacprzak <234758@edu.p.lodz.pl>
2022-05-12 16:14:35 +0200

    Added different images of achievements, pop-ups

--- Flowers/app/src/main/java/com/flowers/ui/achievements/AchievementFragment.kt
+++ Flowers/app/src/main/java/com/flowers/ui/achievements/AchievementFragment.kt
@@ -21,7 +21,31 @@ class AchievementFragment : Fragment() {
     ): View {
         _binding = AchievementFragmentBinding.inflate(inflater, container, false)
         binding.imageButton20.setOnClickListener{
-            Toast.makeText(activity, "123", Toast.LENGTH_LONG).show()
+            Toast.makeText(activity, "You have planted 10 flowers!", Toast.LENGTH_LONG).show()
+        }
+        binding.imageButton18.setOnClickListener{
+            Toast.makeText(activity, "You have planted 50 flowers!", Toast.LENGTH_LONG).show()
+        }
+        binding.imageButton23.setOnClickListener{
+            Toast.makeText(activity, "You have planted 100 flowers!", Toast.LENGTH_LONG).show()
+        }
+        binding.imageButton21.setOnClickListener{
+            Toast.makeText(activity, "You have focused for 3 hours!", Toast.LENGTH_LONG).show()
+        }
+        binding.imageButton17.setOnClickListener{
+            Toast.makeText(activity, "You have focused for 6 hours!", Toast.LENGTH_LONG).show()
+        }
+        binding.imageButton24.setOnClickListener{
+            Toast.makeText(activity, "You have focused for 9 hours!", Toast.LENGTH_LONG).show()
+        }
+        binding.imageButton19.setOnClickListener{
+            Toast.makeText(activity, "You have bought 3 kinds of flowers!", Toast.LENGTH_LONG).show()
+        }
+        binding.imageButton16.setOnClickListener{
+            Toast.makeText(activity, "You have bought 6 kinds of flowers!", Toast.LENGTH_LONG).show()
+        }
+        binding.imageButton22.setOnClickListener{
+            Toast.makeText(activity, "You have bought 9 kinds of flowers!", Toast.LENGTH_LONG).show()
         }
         val root: View = binding.root
         return root
