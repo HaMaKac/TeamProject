7a95cfc57d0a7d8e1cd4253e458119626ffe0dbf
Wiktoria <234762@edu.p.lodz.pl>
2022-05-13 19:56:01 +0200

    deleted "main page"

--- Flowers/app/src/main/java/com/flowers/ui/home/HomeViewModel.kt
+++ Flowers/app/src/main/java/com/flowers/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Welcome to the main page"
+
     }
     val text: LiveData<String> = _text
 }
